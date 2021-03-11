785e32607e79e63f753eddc7b91dc5d8cf2d760e
yulque <double.y.r@gmail.com>
2021-03-10 22:35:34 -0500

    route added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {NewReleasesComponent} from './new-releases/new-releases.component';
+import {ArtistDiscographyComponent} from './artist-discography/artist-discography.component';
+import {AlbumComponent} from './album/album.component';
+import {AboutComponent} from './about/about.component';
+import {NotFoundComponent} from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'newReleases', component: NewReleasesComponent},
+  { path: 'artist', component: ArtistDiscographyComponent},
+  { path: 'album', component: AlbumComponent},
+  { path: 'about', component: AboutComponent},
+  { path: '', redirectTo: 'newReleases', pathMatch: 'full'},
+  { path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
